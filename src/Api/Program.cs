dafbffa21eae8f1c22ff8efb8a0ac53335e41adc
coolbeatz71 <sigmacool@gmail.com>
2025-08-25 19:43:10 +0200

    chore: create Api layer, setup basic foundations

---
+++ src/Api/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, config) =>
+    config.ReadFrom.Configuration(context.Configuration)
+);
+
+// Load environments variables from .env file
+// DotNetEnv.Env.Load();
+// DotNetEnv.Env.TraversePath().Load();
+
+builder.Services.AddAuthorization();
+
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen(c =>
+        {
+            // Add JWT authentication to Swagger
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header uses Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+            
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        }
+    );
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseSerilogRequestLogging();
+app.UseExceptionHandler(_ => { });
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Run();
+
