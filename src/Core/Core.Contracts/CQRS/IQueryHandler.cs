895223f24f2ec09232f2cb82d4f936031264ebaf
coolbeatz71 <sigmacool@gmail.com>
2025-08-25 19:25:32 +0200

    chore: create CQRS core contracts

---
+++ src/Core/Core.Contracts/CQRS/IQueryHandler.cs
@@ -0,0 +1,13 @@
+using MediatR;
+
+namespace _116.Core.Contracts.CQRS;
+
+/// <summary>
+/// Handles queries of type <typeparamref name="TQuery"/> that return a response of type <typeparamref name="TResponse"/>.
+/// Extends <see cref="IRequestHandler{TRequest, TResponse}"/> from MediatR.
+/// </summary>
+/// <typeparam name="TQuery">The type of query to handle.</typeparam>
+/// <typeparam name="TResponse">The type of response returned by the handler.</typeparam>
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TResponse : notnull;
